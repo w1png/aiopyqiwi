687185d618e8ec6d65d345434eb9499ac78479f4
Denis Smirnov <w1png@yandex.ru>
2022-11-26 22:16:45 +0300

    Add docstring

--- aiopyqiwi/types/wallet.py
+++ aiopyqiwi/types/wallet.py
@@ -1,11 +1,19 @@
 import aiohttp
 import re
-import bill
+from random import choice
+from string import ascii_letters, digits
 
 
 class Wallet:
     """
     Класс для работы с кошельком.
+
+    :param token: Токен для доступа к API.
+    :param phone: Номер телефона в формате +7XXXXXXXXXX.
+
+    :raises ValueError: Если номер телефона не в формате +7XXXXXXXXXX.
+
+    :return: Объект класса Wallet.
     """
     def __init__(self, token: str, phone: str) -> None:
         self.token = token
@@ -19,11 +27,14 @@ class Wallet:
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.token}"
         }
+        self.__CHARS = ascii_letters + digits
 
     @property
     async def profile(self) -> dict:
         """
         Получение информации о профиле.
+
+        :return: Словарь с информацией о профиле.
         """
         async with aiohttp.ClientSession() as session:
             async with session.get("https://edge.qiwi.com/person-profile/v1/profile/current", headers=self.__HEADERS) as response:
@@ -33,6 +44,8 @@ class Wallet:
     async def balance(self) -> float:
         """
         Получение баланса кошелька в рублях.
+
+        :return: Баланс кошелька в рублях.
         """
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://edge.qiwi.com/funding-sources/v2/persons/{self.phone}/accounts", headers=self.__HEADERS) as response:
@@ -41,6 +54,11 @@ class Wallet:
     async def get_history(self, rows: int=10, operation: str="IN") -> list:
         """
         Получение истории операций.
+
+        :param rows: Количество операций.
+        :param operation: Тип операции. Возможные значения: ALL, IN, OUT.
+
+        :return: Список с операциями.
         """
         if operation not in ["IN", "OUT", "ALL", "QIWI_CARD"]:
             raise ValueError("operation must be IN, OUT, ALL or QIWI_CARD")
@@ -58,6 +76,12 @@ class Wallet:
     async def transfer(self, amount: float, comment: str, to: str) -> dict:
         """
         Перевод денег на другой кошелек.
+
+        :param amount: Сумма перевода.
+        :param comment: Комментарий к переводу.
+        :param to: Номер телефона в формате +7XXXXXXXXXX.
+
+        :raises ValueError: Если номер телефона не в формате +7XXXXXXXXXX.
         """
         if not re.match(r"^(7|8|\+7)\d{10}$", to):
             raise ValueError("to must be in +7XXXXXXXXXX format")
